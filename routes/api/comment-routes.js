c5dd77cc9fa1f796974c9c03e73277d240d0c16d
Jazelle Pearce <pjazelle@yahoo.com>
2022-07-14 18:07:41 -0500

    first commit

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,23 @@
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
+const router = require('express').Router();
+const { addComment, removeComment } = require('../../controllers/comment-controller');
+
+// /api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commentId').delete(removeComment);
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
+
+module.exports = router;
